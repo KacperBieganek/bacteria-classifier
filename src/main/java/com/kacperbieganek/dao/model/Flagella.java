990f95ba43c0a9f0bf78a46b70711e141cc92811
Kacper Bieganek <KacBieganek@gmail.com>
2018-04-15 16:24:04 +0200

    add dao model

---
+++ src/main/java/com/kacperbieganek/dao/model/Flagella.java
@@ -0,0 +1,31 @@
+package com.kacperbieganek.dao.model;
+
+public class Flagella {
+    private int alphaGene;
+    private int betaGene;
+    private int rank;
+
+    public int getAlfaGene() {
+        return alphaGene;
+    }
+
+    public void setAlfaGene(int alphaGene) {
+        this.alphaGene = alphaGene;
+    }
+
+    public int getBetaGene() {
+        return betaGene;
+    }
+
+    public void setBetaGene(int betaGene) {
+        this.betaGene = betaGene;
+    }
+
+    public int getRank() {
+        return rank;
+    }
+
+    public void setRank(int rank) {
+        this.rank = rank;
+    }
+}
